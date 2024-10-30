6b12c6b9163f5e5dd87b026eba34aa9bde1b0cb2
Caio Cesar <caiocesarvieiracavalcanti@gmail.com>
2024-10-30 17:17:29 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,26 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection conn;
+	
+	// Dependência para a inicialização do atributo "conn"
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +41,54 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName " +
+					"FROM seller INNER JOIN department " +
+					"ON seller.DepartmentId = department.Id " +
+					"WHERE seller.Id = ?"
+					);
+			// Substituindo o placeholder com o id do parâmetro
+			st.setInt(1, id);
+			
+			// Execução da consulta do Statement, retornando um ResultSet
+			rs = st.executeQuery();
+			
+			// Verificar se existe algum dado na consulta (se retornar valor 0 - false, não possui)
+			if(rs.next()) {
+				// Criando e preenchendo os atributos do objeto Department (igual ao UML disposto no resumo)
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				
+				// Criando e preenchendo os atributos do objeto Seller
+				Seller seller = new Seller();
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				seller.setBirthDate(rs.getDate("BirthDate"));
+				
+				// Estabelecendo a associação entre os objetos Department e Seller
+				seller.setDepartment(dep);
+				
+				return seller;
+				
+			}
+			
+			return null;
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+			// CUIDADO! Não fechamos a conexão com o banco de dados, por ser uma classe que 
+			// possui outros métodos de acesso a ele
+		}
 	}
 
 	@Override
