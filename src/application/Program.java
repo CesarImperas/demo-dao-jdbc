8132dbeaaf8a5a0fc893a2ec18ccace12ccf8851
Caio Cesar <caiocesarvieiracavalcanti@gmail.com>
2024-10-30 13:07:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,10 @@ public class Program {
 		Department dp = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+		
+		
+		// Dessa forma, o programa não conhece a implementação, somente a interface
+		SellerDao sellerDao = DaoFactory.createrSellerDao();
 		
 		System.out.println(seller);
 	}
